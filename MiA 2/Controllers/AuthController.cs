2a8ff10476ecabf0626c13b297e0a411057717a8
Marusya <mariapastuh719@gmail.com>
2025-11-20 13:53:29 +0200

    Test base CI/CD

--- MiA 2/Controllers/AuthController.cs
+++ MiA 2/Controllers/AuthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 
 namespace Controllers;
-
+//Testing comment
 [ApiController]
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
